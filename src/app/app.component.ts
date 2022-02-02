52920be13858c6ca45bfabf284542113f01dfe33
enomilan <andrewmusta@gmail.com>
2022-02-02 10:08:48 -0600

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,29 @@ export class AppComponent {
   title = 'ang';
   counter = 1
   greet = ''
-
+  display = 'true'
   
 
   
+  true() {
 
+
+
+  }
     
     
   increment() {
 
-      if (this.counter % 3 == 0) this.greet = 'American'
-
+      if (this.counter % 3 == 0) this.greet = 'American'  
+            else this.greet = ''
+         
 
       if (this.counter % 5 === 0) this.greet = 'Medical'
-
+        
 
       if (this.counter % 3 === 0 && this.counter % 5 === 0) this.greet = 'Opportunities'
+        
+      
 
       if (this.counter === 100) this.counter = 0;
 
