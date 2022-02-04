6ac3c94677d22a235bb1a1a177a0f3aad1698b91
enomilan <andrewmusta@gmail.com>
2022-02-04 00:14:02 -0600

    counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,18 +11,17 @@ export class AppComponent {
   greet = ''
   toggle = true
   
-
+  amVal = 2
+  medVal = 4
+  opVal = 14
   
-  display() {
-
-      
-  }
-    
+     
     
   increment() {
 
-      if (this.counter % 3 === 0) 
-          {
+    if (this.counter  === this.amVal) 
+          { 
+            this.amVal += 3;
             this.toggle = !this.toggle; 
             this.greet = 'American'; }  
             
@@ -32,18 +31,50 @@ export class AppComponent {
               }
          
 
-      if (this.counter % 5 === 0) 
-        { this.toggle = !this.toggle;
+      if (this.counter === this.medVal)
+
+        { this.medVal += 5;
+          this.toggle = !this.toggle;
           this.greet = 'Medical' }
         
 
-      if (this.counter % 3 === 0 && this.counter % 5 === 0) 
-        { this.toggle = !this.toggle;
-          this.greet = 'Opportunities'}
+      if (this.counter === this.opVal) 
+        { this.opVal += 15;
+          this.toggle = !this.toggle;
+          this.greet = 'Opportunities'}   
+
+     if (this.counter === 100) 
+         {this.counter = 0;
+          this.amVal = 2;
+          this.medVal = 4;
+          this.opVal = 14; } 
+          
+          
+      //Modulo solution
+          
+      // if (this.counter % 3 === 0) 
+      //     {
+      //       this.toggle = !this.toggle; 
+      //       this.greet = 'American'; }  
+            
+      // else {
+      //         this.toggle = true
+      //         this.greet = ''
+      //         }
+         
+
+      // if (this.counter % 5 === 0) 
+      //   { this.toggle = !this.toggle;
+      //     this.greet = 'Medical' }
+        
+
+      // if (this.counter % 3 === 0 && this.counter % 5 === 0) 
+      //   { this.toggle = !this.toggle;
+      //     this.greet = 'Opportunities'}
         
       
 
-      if (this.counter === 100) this.counter = 0;
+      
 
     	this.counter++;
     
