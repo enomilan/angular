fdb1efeecf1e3605ada3106507ddfcbe143a430e
enomilan <andrewmusta@gmail.com>
2022-02-17 22:31:51 -0600

    front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,7 +52,25 @@ export class AppComponent {
           
       //Modulo solution
           
-?
+      if (this.counter % 3 === 0) 
+          {
+            this.toggle = !this.toggle; 
+            this.greet = 'Forza'; }  
+            
+      else {
+              this.toggle = true
+              this.greet = ''
+              }
+         
+
+      if (this.counter % 5 === 0) 
+        { this.toggle = !this.toggle;
+          this.greet = 'Milan' }
+        
+
+      if (this.counter % 3 === 0 && this.counter % 5 === 0) 
+        { this.toggle = !this.toggle;
+          this.greet = 'Campione'}
         
       
 
